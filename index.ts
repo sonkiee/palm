e237590cba85c1a43acceb95867a9eeecad996ff
sonkiee <annagu.kennedy@gmail.com>
2025-08-12 15:57:15 +0000

    Aug 12, 2025, 4:57 PM

--- index.ts
+++ index.ts
@@ -1,9 +1,26 @@
 import { Navigation } from "react-native-navigation";
-import { registerRootComponent } from "expo";
+// import { registerRootComponent } from "expo";
 
 import App from "./App";
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
-registerRootComponent(App);
+// registerRootComponent(App);
+
+Navigation.registerComponent("com.myApp.WelcomeScreen", () => App);
+Navigation.events().registerAppLaunchedListener(() => {
+  Navigation.setRoot({
+    root: {
+      stack: {
+        children: [
+          {
+            component: {
+              name: "com.myApp.WelcomeScreen",
+            },
+          },
+        ],
+      },
+    },
+  });
+});
